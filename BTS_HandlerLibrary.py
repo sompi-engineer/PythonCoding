2044d0845a0567e89d44269ef7a3efb2757a0141
asuomela <antti.suomela@keysight.com>
2024-01-23 16:10:42 +0200

    Added dictionary for parameter index

--- BTS_HandlerLibrary.py
+++ BTS_HandlerLibrary.py
@@ -19,9 +19,7 @@ REQUIRED_PARAMETERS = ['SYSTEM', 'CELL', 'SITE']
 def read(list):
     filename = input("Enter the file name and path: ")
     header_list = []
-    system_no = 0
-    site_no = 0
-    cell_no = 0
+    parameter_no = {"system_no": 0, "site_no": 0, "cell_no": 0}
     number = 0
     try:
         file = open(filename, "r", encoding="utf-8")
@@ -36,11 +34,11 @@ def read(list):
         # Resolve which column number contains required parameters
         for parameter in header_list:
             if parameter == "SYSTEM":
-                system_no = number
+                parameter_no["system_no"] += number
             elif parameter == "SITE":
-                site_no = number
+                parameter_no["site_no"] += number
             elif parameter == "CELL":
-                cell_no = number
+                parameter_no["cell_no"] += number
             number += 1
         # Read through the file and append wanted values to class
         while True:
@@ -49,9 +47,9 @@ def read(list):
                 break
             column = line.split(SEPARATOR)
             cell = CELLS()
-            cell.system = column[system_no]
-            cell.site = column[site_no]
-            cell.cell = column[cell_no]
+            cell.system = column[parameter_no["system_no"]]
+            cell.site = column[parameter_no["site_no"]]
+            cell.cell = column[parameter_no["cell_no"]]
             list.append(cell)
         file.close()
     except Exception:
